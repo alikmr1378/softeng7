3477512314b735ba0a40c22d16157d376e302026
alikmr <alikarimlo1999@gmail.com>
2023-05-19 22:38:42 +0330

    facade

---
+++ src/main/java/codeGenerator/MemoryFacade.java
@@ -0,0 +1,24 @@
+package codeGenerator;
+
+
+public class MemoryFacade {
+    private static MemoryFacade memoryFacade = null;
+    private Memory memory = new Memory();
+
+    private MemoryFacade() {
+
+    }
+
+    public void setMemory(Memory memory) {
+        this.memory = memory;
+    }
+
+    public Memory getMemory() {
+        return memory;
+    }
+
+    public static MemoryFacade getMemoryFacade() {
+        return (memoryFacade == null) ? memoryFacade = new MemoryFacade() : memoryFacade;
+    }
+
+}
